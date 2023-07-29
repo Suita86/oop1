668b64f4e5eb194c5f50b47508f43b8f8ec34600
Suita86 <125517340+Suita86@users.noreply.github.com>
2023-07-30 00:07:25 +0300

    Update Familytree.java

--- src/Familytree.java
+++ src/Familytree.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Familytree {
+// Модель дерева семьи
+public class FamilyTree {
+    private Person root;
+
+    public FamilyTree(Person root) {
+        this.root = root;
+    }
+
+    public List<Person> getAllChildren(Person parent) {
+        List<Person> children = new ArrayList<>();
+        if (parent.getChildren().isEmpty()) {
+            return children;
+        } else {
+            for (Person child : parent.getChildren()) {
+                children.add(child);
+                children.addAll(getAllChildren(child));
+            }
+            return children;
+        }
+    }
 }

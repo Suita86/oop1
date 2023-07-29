833776f950d1a3e8a955cae252cb2df3f55e2732
Suita86 <125517340+Suita86@users.noreply.github.com>
2023-07-30 00:10:07 +0300

    Update GenealogyResearch.java

--- src/GenealogyResearch.java
+++ src/GenealogyResearch.java
@@ -1,2 +1,21 @@
-public class Genealogy {
+public class GenealogyResearch {
+    private FamilyTree familyTree;
+
+    public GenealogyResearch(FamilyTree familyTree) {
+        this.familyTree = familyTree;
+    }
+
+    public List<Person> getAllChildren(Person parent) {
+        return familyTree.getAllChildren(parent);
+    }
+
+    public List<Person> getAllDescendants(Person ancestor) {
+        List<Person> descendants = new ArrayList<>();
+        List<Person> children = getAllChildren(ancestor);
+        descendants.addAll(children);
+        for (Person child : children) {
+            descendants.addAll(getAllDescendants(child));
+        }
+        return descendants;
+    }
 }
